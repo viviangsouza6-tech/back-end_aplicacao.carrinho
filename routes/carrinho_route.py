54d09089c2a043349d163da8daf9be2d9b5f6cc0
vivian <vivian.g.souza6@.aluno.senai.br>
2026-09-09 20:08:02 -0300

    modified:   routes/carrinho_route.py

--- routes/carrinho_route.py
+++ routes/carrinho_route.py
@@ -1,32 +1,50 @@
 from fastapi import APIRouter
 
+from schemas.carrinho_schema import (
+    CarrinhoProdutoSchema,
+    CarrinhoQuantidadeSchema
+)
+
+from services.carrinho_service import CarrinhoService
+
+
 router = APIRouter(
     prefix="/carrinho",
     tags=["Carrinho"]
 )
 
 
+carrinho_service = CarrinhoService()
+
+
 @router.post("/{idpedido}/produtos")
 def adicionar_produto(
     idpedido: int,
-    idproduto: int,
-    quantidade: int
+    dados: CarrinhoProdutoSchema
 ):
-    pass
+    return carrinho_service.adicionar_produto(
+        idpedido,
+        dados.idproduto,
+        dados.quantidade
+    )
 
 
 @router.get("/{idpedido}")
 def listar_carrinho(idpedido: int):
-    pass
+    return carrinho_service.listar_produtos(idpedido)
 
 
 @router.put("/{idpedido}/produtos/{idproduto}")
 def alterar_quantidade(
     idpedido: int,
     idproduto: int,
-    quantidade: int
+    dados: CarrinhoQuantidadeSchema
 ):
-    pass
+    return carrinho_service.alterar_quantidade(
+        idpedido,
+        idproduto,
+        dados.quantidade
+    )
 
 
 @router.delete("/{idpedido}/produtos/{idproduto}")
@@ -34,9 +52,12 @@ def remover_produto(
     idpedido: int,
     idproduto: int
 ):
-    pass
+    return carrinho_service.remover_produto(
+        idpedido,
+        idproduto
+    )
 
 
 @router.get("/{idpedido}/total")
 def calcular_total(idpedido: int):
-    pass
+    return carrinho_service.calcular_total(idpedido)
